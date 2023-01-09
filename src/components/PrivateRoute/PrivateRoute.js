b4f1dafd167b3eda43927c5bc3ac5cef1185564d
leshazimoglyad <102586567+leshazimoglyad@users.noreply.github.com>
2023-01-09 22:55:40 +0200

    hfdfhjgv

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,24 +1,4 @@
-//import { useSelector } from 'react-redux';
-//import { Navigate } from 'react-router-dom';
-//import { selectIsLoggedIn } from 'redux/auth/slice';
 
-//export function PrivateRoute({ children, redirectTo = '/', ...routeProps }) {
-//  const isLoggedIn = useSelector(selectIsLoggedIn);
-//  return (
-//    <div {...routeProps}>
-//      {isLoggedIn ? <div>{children}</div> : <Navigate to={redirectTo} />}
-//    </div>
-//  );
-//}
-///////////////////////////////////////////
-//import { useSelector } from 'react-redux';
-//import { Navigate } from 'react-router-dom';
-
-//export const PrivateRoute = ({ children, redirectTo = '/' }) => {
-//  const token = useSelector(state => state.auth.token);
-//  return token ? children : <Navigate to={redirectTo} />;
-//};
-///////////////////////////////////////////////////////////
 import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectIsLoggedIn, selectIsRefreshing } from 'redux/auth/slice';
--- src/redux/filter/filterReduser.js
+++
@@ -1,7 +0,0 @@
-import { filterContact } from './filterActions';
-
-import { createReducer } from '@reduxjs/toolkit';
-
-export const filter = createReducer('', {
-  [filterContact.type]: (_, action) => action.payload.toLowerCase(),
-});
